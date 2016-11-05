5cafb032a87af5b2249797794a3cd37a2a90d2fc
Beau Carnes <beaucarnes@gmail.com>
2016-11-05 23:33:42 +0000

    Fixing authentication.

--- routes/googlecallback.js
+++ routes/googlecallback.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var passportGoogle = require('../auth/google');
 
 router.get('/',
-  passportGoogle.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }),
+  passportGoogle.authenticate('google', { successRedirect: '/', failureRedirect: '/' }),
   function(req, res) {
     // Successful authentication
     res.json(req.user);
