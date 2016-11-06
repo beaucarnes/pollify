88efd44d1f8a8f3d28bbd06a98042a10ffaa84d1
Beau Carnes <beaucarnes@gmail.com>
2016-11-06 00:50:27 +0000

    Update wording and auth bug.

--- routes/googlecallback.js
+++ routes/googlecallback.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var passportGoogle = require('../auth/google');
 
 router.get('/',
-  passportGoogle.authenticate('google', { successRedirect: '/', failureRedirect: '/' }),
+  passportGoogle.authenticate('google', { successRedirect: '/', failureRedirect: '/auth/google' }),
   function(req, res) {
     res.json(req.user);
   });
